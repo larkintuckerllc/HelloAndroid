ad1cb020e215bed877949e55f1b57084cce5085c
John Tucker <john@larkintuckerllc.com>
2015-07-31 15:16:41 -0400

    Initial commit

---
+++ app/src/main/java/com/larkintuckerllc/helloandroid/MainActivity.java
@@ -0,0 +1,37 @@
+package com.larkintuckerllc.helloandroid;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_hello) {
+            Snackbar
+                    .make(findViewById(R.id.snackbarPosition), R.string.snackbar_text, Snackbar.LENGTH_LONG)
+                    .show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
