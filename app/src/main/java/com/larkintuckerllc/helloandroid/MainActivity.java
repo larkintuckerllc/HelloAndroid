ac742ae6a4be716f2c0a3896142c9c300fd2f709
John Tucker <john@larkintuckerllc.com>
2015-08-01 17:38:02 -0400

    Fixing some warnings.

--- app/src/main/java/com/larkintuckerllc/helloandroid/MainActivity.java
+++ app/src/main/java/com/larkintuckerllc/helloandroid/MainActivity.java
@@ -2,19 +2,38 @@ package com.larkintuckerllc.helloandroid;
 
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<String> things;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        things = new ArrayList<String>();
+        things.add("one");
+        things.add("two");
+        things.add("three");
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        RecyclerView thingRV = (RecyclerView) findViewById(R.id.things);
+        thingRV.setHasFixedSize(true);
+        thingRV.setLayoutManager(new LinearLayoutManager(this));
+        thingRV.setAdapter(new ThingAdapter(things));
     }
 
     @Override
@@ -34,4 +53,41 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public class ThingViewHolder extends RecyclerView.ViewHolder {
+
+        String id;
+        TextView name;
+
+        ThingViewHolder(View view) {
+            super(view);
+            name = (TextView)view.findViewById(R.id.name);
+        }
+
+    }
+
+    public class ThingAdapter extends RecyclerView.Adapter<ThingViewHolder> {
+
+        List<String> things;
+
+        ThingAdapter(List<String> things) {
+            this.things = things;
+        }
+
+        public ThingViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.thing, viewGroup, false);
+            return new ThingViewHolder(view);
+        }
+
+        public int getItemCount() {
+            return things.size();
+        }
+
+        public void onBindViewHolder(ThingViewHolder viewHolder, int i) {
+            viewHolder.id = things.get(i);
+            viewHolder.name.setText(things.get(i));
+        }
+
+    }
+
 }
