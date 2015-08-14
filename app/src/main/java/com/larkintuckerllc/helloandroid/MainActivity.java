1aafae2b43cd378388d9dd5c4806041f13954a53
John Tucker <john@larkintuckerllc.com>
2015-08-14 13:24:58 -0400

    Base install of Fabric.

--- app/src/main/java/com/larkintuckerllc/helloandroid/MainActivity.java
+++ app/src/main/java/com/larkintuckerllc/helloandroid/MainActivity.java
@@ -10,12 +10,15 @@ import android.support.v7.widget.Toolbar;
 import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity implements MainFragment.OnThingSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
